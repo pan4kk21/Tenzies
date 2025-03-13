af14832eae3bd6c9fa713a24b74ac18b40b06f9f
Pavel Melnik <pan4kkpan4@gmail.com>
2025-03-13 12:38:47 +0100

    Generate random dice

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import Die from "./components/Die.jsx"
+import { useState } from "react";
 
 export default function App() {
+
+    const [diceNumbers, setDiceNumbers] = useState(generateDiceNumbers)
+
+    function generateDiceNumbers() {
+        return new Array(10)
+            .fill(0)
+            .map(() => Math.ceil(Math.random() * 6))
+    }
+
+    const diceComponents = diceNumbers.map(number => <Die value={number} />)
+
     return (
         <main>
             <div className="dice-container">
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
+                {diceComponents}
             </div>
         </main>
     )
